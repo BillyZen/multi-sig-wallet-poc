91c654e195683b1ae8b195ecdebacc07bfa31a36
BillyZen <willjayandtay@hotmail.co.uk>
2023-06-21 15:53:24 +0100

    fixed provider issue with hardhat and sepolia

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,15 +1,16 @@
 const API_KEY = process.env.API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const API_URL = process.env.API_URL;
 
 const { ethers } = require("hardhat");
 const contract = require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json");
 
 // provider - Alchemy
-const alchemyProvider = new ethers.providers.AlchemyProvider([
-  (network = "sepolia"),
-  API_KEY,
-]);
+const alchemyProvider = new ethers.providers.JsonRpcProvider(
+  API_URL,
+  "sepolia"
+);
 
 // signer - you
 const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
