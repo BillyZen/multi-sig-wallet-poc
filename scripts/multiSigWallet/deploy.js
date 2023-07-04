2cf607f672ba37a90d90a3f93cfbe5d521838060
BillyZen <willjayandtay@hotmail.co.uk>
2023-07-04 11:16:10 +0100

    deployed multi sig but cant verify

--- scripts/multiSigWallet/deploy.js
+++ scripts/multiSigWallet/deploy.js
@@ -3,7 +3,13 @@ const { ethers } = require("hardhat");
 async function main() {
   const MultiSigWallet = await ethers.getContractFactory("MultiSigWallet");
 
-  const multi_sig_wallet = await MultiSigWallet.deploy();
+  const multi_sig_wallet = await MultiSigWallet.deploy(
+    [
+      "0x1D7F6E0FC483198caD49D6c2C560b48c9e825d2e",
+      "0xa428434AAd9455C2859eD018eD0C03f504A9431d",
+    ],
+    2
+  );
   console.log("Contract deployed to address: ", multi_sig_wallet.address);
 }
 
